70149c6223d5e77a7056c7e0ba854927429d9c16
John Doe <lebanner1087@gmail.com>
2025-04-17 13:36:04 -0400

    p

--- app/api/stripe/checkout/route.ts
+++ app/api/stripe/checkout/route.ts
@@ -9,6 +9,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
   apiVersion: "2025-01-27.acacia", // Use your preferred API version
 });
 
+
 export async function POST(req: NextRequest) {
   try {
     const session = await getServerSession(authOptions);
