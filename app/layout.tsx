341e9eaebdfa7ba77f4c732f2cd71010bc1c2e14
John Doe <lebanner1087@gmail.com>
2025-12-04 16:10:31 -0500

    ok

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,7 @@ import Footer from '@/components/ui/footer';
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Job Matcher',
+  title: 'Workr',
   description: 'Match with job postings',
    icons: {
     icon: '/favicon.ico',
